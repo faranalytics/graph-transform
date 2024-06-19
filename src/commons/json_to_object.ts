dedc6e4bf291be43a4a267ddbd71cd91f4bfbec3
adamjpatterson <adam@farar.net>
2024-06-19 17:09:06 -0400

    more

--- src/commons/json_to_object.ts
+++ src/commons/json_to_object.ts
@@ -7,16 +7,28 @@ export interface JSONToObjectOptions {
 
 export class JSONToObject<OutT = object> extends Transform<string, OutT> {
 
+    public _queue: string = '';
+
     constructor({ reviver }: JSONToObjectOptions = {}, options?: s.TransformOptions) {
         super(new s.Transform({
             ...options, ...{
                 writableObjectMode: true,
                 readableObjectMode: true,
                 transform: async (chunk: string, _encoding: BufferEncoding, callback: s.TransformCallback) => {
-                    callback(null, JSON.parse(chunk, reviver));
+                    try {
+                        const obj = JSON.parse(this._queue + chunk, reviver);
+                        callback(null, obj);
+                        this._queue = '';
+                    }
+                    catch(err) {
+                        this._queue = this.queue + chunk;
+                        callback();
+                    }
                 }
             }
         }));
+
+        this._queue = '';
     }
 
     async write(data: string): Promise<void> {
--- test/src/index.ts
+++ test/src/index.ts
@@ -1,7 +1,11 @@
 import * as net from 'node:net';
 import { Transform, StringToBuffer, BufferToString, ObjectToJSON, JSONToObject } from 'graph-transform';
 import { TemporalTransform } from './temporal_transform.js';
-import { NullTransform } from './null_transform.js';
+// import { NullTransform } from './null_transform.js';
+
+class Greeter {
+    public greeting: string = 'Hello, World!'.repeat(1e4);
+}
 
 async function test1() {
 
@@ -9,9 +13,10 @@ async function test1() {
     const temporalTransform2 = new TemporalTransform({ time: 1000 });
     const objectToJSON1 = new ObjectToJSON();
     const objectToJSON2 = new ObjectToJSON();
-    const jsonToObject = new JSONToObject();
+    const jsonToObject = new JSONToObject<Greeter>();
     const stringToBuffer = new StringToBuffer();
     const bufferToString = new BufferToString();
+    const stdout = new Transform<string, never>(process.stdout);
 
     net.createServer((socket: net.Socket) => socket.pipe(socket)).listen(3000);
     const socket = net.createConnection({ port: 3000 });
@@ -26,7 +31,7 @@ async function test1() {
                         bufferToString.connect(
                             jsonToObject.connect(
                                 objectToJSON2.connect(
-                                    new Transform<string, never>(process.stdout)
+                                    stdout
                                 )
                             )
                         )
@@ -36,37 +41,39 @@ async function test1() {
         )
     );
 
-    transform.write({ 'greeting': 'Hello, World!' });
+    transform.write(new Greeter());
 }
 
-async function test2() {
-
-    const stringToBuffer = new StringToBuffer();
-    const bufferToString = new BufferToString();
-    const nullTransform = new NullTransform();
-
-    net.createServer((socket: net.Socket) => socket.pipe(socket)).listen(3001);
-    const socket = net.createConnection({ port: 3001 });
-    await new Promise((r, e) => socket.once('connect', r).once('error', e));
-    const socketHandler = new Transform<Buffer, Buffer>(socket);
-
-    const transform = stringToBuffer.connect(
-        socketHandler.connect(
-            bufferToString.connect(
-                nullTransform
-            )
-        )
-    );
-
-    transform.write('0'.repeat(1e6));
-    transform.write('0'.repeat(1e6));
-}
 
 function main() {
     test1();
-    test2();
+    // test2();
 }
 
 main();
+
+// async function test2() {
+
+//     const stringToBuffer = new StringToBuffer();
+//     const bufferToString = new BufferToString();
+//     const nullTransform = new NullTransform();
+
+//     net.createServer((socket: net.Socket) => socket.pipe(socket)).listen(3001);
+//     const socket = net.createConnection({ port: 3001 });
+//     await new Promise((r, e) => socket.once('connect', r).once('error', e));
+//     const socketHandler = new Transform<Buffer, Buffer>(socket);
+
+//     const transform = stringToBuffer.connect(
+//         socketHandler.connect(
+//             bufferToString.connect(
+//                 nullTransform
+//             )
+//         )
+//     );
+
+//     transform.write('0'.repeat(1e6));
+//     transform.write('0'.repeat(1e6));
+// }
+
 
 
