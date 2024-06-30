698fcef317a138f10ed8fa05cf238b3be29ffb1d
adamjpatterson <adam@farar.net>
2024-06-30 07:50:27 -0400

    more

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { BufferToString } from './commons/buffer_to_string.js';
 import { ConsoleHandler } from './commons/console_handler.js';
 import { SocketHandler } from './commons/socket_handler.js';
 import { AnyToTest } from './commons/any_to_test.js';
+import { AnyToVoid } from './commons/any_to_void.js';
 
 export {
     Transform,
@@ -14,6 +15,7 @@ export {
     ConsoleHandler,
     SocketHandler,
     AnyToTest,
+    AnyToVoid,
     $stream,
     $queue,
     $rhsConnected,
